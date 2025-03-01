084e1fa366cf83417628a505d1b7462d341edd0f
paezdev <jean_paez@hotmail.es>
2025-03-01 17:47:06 -0500

    Se quitó el TODO

--- src/load.py
+++ src/load.py
@@ -11,9 +11,5 @@ def load(data_frames: Dict[str, DataFrame], database: Engine):
         data_frames (Dict[str, DataFrame]): A dictionary with keys as the table names
         and values as the dataframes.
     """
-    # TODO: Implementa esta función. Por cada DataFrame en el diccionario, debes
-    # usar pandas.DataFrame.to_sql() para cargar el DataFrame en la base de datos
-    # como una tabla.
-    # Para el nombre de la tabla, utiliza las claves del diccionario `data_frames`.
     for table_name, df in data_frames.items():
         df.to_sql(table_name, con=database, if_exists="replace")
